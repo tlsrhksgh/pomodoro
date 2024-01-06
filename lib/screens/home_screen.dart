c87b082f41a88c66fc1b3681d3bbc98094943d5b
tlsrhksgh <sin99145@naver.com>
2024-01-06 15:10:02 +0900

    pomodoro 화면 구성 완료

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,21 +17,66 @@ class _HomeScreenState extends State<HomeScreen> {
             Flexible(
               flex: 1,
               child: Container(
+                alignment: Alignment.bottomCenter,
                 child: Text(
                   '25:00',
                   style: TextStyle(
                     color: Theme.of(context).cardColor,
+                    fontSize: 89,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
             ),
             Flexible(
               flex: 4,
-              child: Container(),
+              child: Center(
+                child: IconButton(
+                  icon: const Icon(Icons.play_circle_outline),
+                  onPressed: () {},
+                  iconSize: 98,
+                  color: Theme.of(context).cardColor,
+                ),
+              ),
             ),
             Flexible(
               flex: 1,
-              child: Container(),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Pomodoros',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .displayLarge!
+                                    .color,
+                              )),
+                          Text(
+                            '0',
+                            style: TextStyle(
+                              fontSize: 50,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge!
+                                  .color,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ));
